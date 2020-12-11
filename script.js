392f8d81a2a2e85b598a7849e1e14603718ac028
Veronika-Clever11 <71881291+Veronika-Clever11@users.noreply.github.com>
2020-12-11 15:10:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-function printtext() {
-  console.log(text.value);
-}
-text.addEventListener("input", printtext);
+let input = document.querySelector('input');
+input.addEventListener('click', (event) => {
+  event.stopPropagation();
+  let inputValue = event.target.value;
+  console.log(inputValue);
+});
+let inputValue = prompt("Введите сумму в долларах");
+alert(inputValue + " долларов = " + inputValue * 77 + " рублей");
